aabe2a46c745f1b29124f01c710f6cd19f856fc6
FedericoGaubeca <47962250@est.ort.edu.ar>
2024-06-10 15:38:57 -0300

    Initial commit

---
+++ Assets/Scripts/PickUpScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpScript : MonoBehaviour
+{
+    public Text puntajeTxt;
+
+    int puntaje = 0;
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Food"))
+        { 
+            puntaje += 1;
+            puntajeTxt.text = puntaje.ToString();
+            Destroy(col.gameObject);
+        }
+    }
+
+}
